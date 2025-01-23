df668edb9ae46b29da09de268f1897bd4bd83084
BruceBest <BruceLvJC@outlook.com>
2025-01-22 21:15:50 -0500

    remove unused lib

--- src/DataExplore/StatTable.js
+++ src/DataExplore/StatTable.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { BACKEND, filterLabels } from "../consts";
-import axios from "axios";
 import "./StatTable.css";
 
 const StatTable = () => {
